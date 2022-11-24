c9806d423e6669c78a1fb8806cd0c0f7c2855ad9
roshmaep <roshmaep1999@gmail.com>
2022-11-24 23:12:07 +0530

    user reg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { BookViewComponent } from './book-view/book-view.component';
 import { UserLoginComponent } from './user-login/user-login.component';
+import { UserRegComponent } from './user-reg/user-reg.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminLoginComponent,
     BookViewComponent,
-    UserLoginComponent
+    UserLoginComponent,
+    UserRegComponent
   ],
   imports: [
     BrowserModule,
