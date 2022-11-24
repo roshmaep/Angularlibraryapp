7cc5e25389d0a41859105a16bd2748c4930a1c3f
roshmaep <roshmaep1999@gmail.com>
2022-11-24 23:06:11 +0530

    user login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { BookViewComponent } from './book-view/book-view.component';
+import { UserLoginComponent } from './user-login/user-login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminLoginComponent,
-    BookViewComponent
+    BookViewComponent,
+    UserLoginComponent
   ],
   imports: [
     BrowserModule,
