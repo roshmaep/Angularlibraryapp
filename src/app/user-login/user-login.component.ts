a1b2d41da6d12a54469c49fd741c9cb35f110b54
roshmaep <roshmaep1999@gmail.com>
2022-11-28 17:33:15 +0530

    user validation

--- src/app/user-login/user-login.component.ts
+++ src/app/user-login/user-login.component.ts
@@ -14,5 +14,11 @@ readValues=()=>
 {
   let data:any={"username":this.username,"password":this.password}
   console.log(data)
+
+  if (this.username=="admin" && this.password=="12345") {
+    alert("valid login")
+  } else {
+    alert("invalid creditianals")
+  }
 }
 }
